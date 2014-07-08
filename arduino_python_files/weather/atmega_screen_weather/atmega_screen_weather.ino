d5e056e9e3d238d8a17e0aaa7af452dab7d7cdfb
facelessloser <facelessloser@gmail.com>
2014-07-08 21:26:21 +0100

    commented code

--- arduino_python_files/weather/atmega_screen_weather/atmega_screen_weather.ino
+++ arduino_python_files/weather/atmega_screen_weather/atmega_screen_weather.ino
@@ -3,15 +3,11 @@
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 #include "RTClib.h"
 
-RTC_DS1307 rtc;
-
-long waitUntiltime = 0;
-
 String readString; // readstring is for collecting the serial buffer code
 String temp_c;  // string that stores temp
 String feels;  // string that stores feels like temp
 String weather;  // string that stores weather forcast
-String lowest;  // string that stores lowest temp
+String lowest_temp;  // string that stores lowest temp
 String wind;  // string that stores wind speed
 String lowest_time;  // Time last lowest
 String highest_temp;  // string that stores highest temp
@@ -29,19 +25,19 @@ byte degree[8] = {
   B00000
 };
 
-// Button stuff
-const int button = 5;
+// Button
+const int button = 5; // Set button to pin 5
 int buttonPushCounter1 = 8;    // counts the button pushes
 int buttonState1 = 0;    // tracks the button state
 int lastButtonState1 = 0;    // last state of the button
 
-// Debounce stuff
+// Debounce
 int buttonState;             // the current reading from the input pin
-int lastButtonState = LOW;
+int lastButtonState = LOW;  // last button state set to low
 long lastDebounceTime = 0;  // the last time the output pin was toggled
 long debounceDelay = 50;    // the debounce time; increase if the output flickers
 
-// Timing for the display cycle
+// Timing for the display cycle times
 long waitUntilcycle1 = 0;
 long waitUntilcycle2 = 5000;
 long waitUntilcycle3 = 10000;
@@ -50,110 +46,109 @@ long waitUntilcycle5 = 20000;
 long waitUntilcycle6 = 25000;
 long waitUntilcycle7 = 30000;
 
-// Sets the led pins
-int redPin = 12;
-int greenPin = 11;
-int bluePin = 10;
+// RGB LED
+int redPin = 12; // Red led set to pin 12
+int greenPin = 11; // Green led set to pin 11
+int bluePin = 10; // Blue led set to pin 10
 //#define COMMON_ANODE
 
-// RTC stuff
+// RTC 
 const int DS1307 = 0x68; // Address of DS1307 see data sheets
 const char* days[] =
-{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}; // converts the numbers from the days of week to names
 
 // Initializes all values: 
-byte second = 0;
-byte minute = 0;
-byte hour = 0;
-byte weekday = 0;
-byte monthday = 0;
-byte month = 0;
-byte year = 0;
+byte second = 0; // Setting up bytes for seconds
+byte minute = 0; // Setting up bytes for minutes
+byte hour = 0; // Setting up bytes for hours
+byte weekday = 0; // Setting up bytes for weekday
+byte monthday = 0; // Setting up bytes for monthday
+byte month = 0; // Setting up bytes for month
+byte year = 0;  // Setting up bytes for year
 
 
 void setup() {
-  Wire.begin();
-  lcd.backlight();
-  lcd.init();
-  lcd.begin(16, 2);
-  pinMode(button, INPUT);
-  digitalWrite(button, HIGH);
+  Wire.begin(); // Start the wire liberty
+  lcd.backlight(); // Turn on the lcd backlight
+  lcd.init(); // Start up the lcd
+  lcd.begin(16, 2); // Set up the lcd to have 16 char on 2 lines
+  pinMode(button, INPUT); // Set the button as input
+  digitalWrite(button, HIGH); // initiate the internal pull up resistor
   //lcd.createChar(0, degree);
-  Serial.begin(9600);
-  Serial.println("Waiting");
+  Serial.begin(9600); // Begin serial at 9600 baud
+  Serial.println("Waiting"); // Print to serial 
 }
 
 void loop() {
-  //Serial.println(highest_temp);
-  lcd.setCursor(0,0);
-  if (Serial.available())  {
+  lcd.setCursor(0,0); // Set lcd cursor to the start of the first row
+  if (Serial.available())  { // Check to see if serial is available
     char c = Serial.read();  //gets one byte from serial buffer
     
-    if (c == ';') {
-      if (readString.length() >0) {
-        if (readString.startsWith("#1")) {
-          temp_c = readString.substring(2);
-          Serial.println(temp_c);
+    if (c == ';') { // Checks to see if c contains ;
+      if (readString.length() >0) { // Checks that the length of readString is over 0
+        if (readString.startsWith("#1")) { // Checks readString if it starts with #1
+          temp_c = readString.substring(2); // Adds the readString less the first 2 characters to temp_c
+          Serial.println(temp_c); // Prints temp_c to serial
         }
         
-        else if (readString.startsWith("#2")) {
-          feels = readString.substring(2);
-          Serial.println(feels);
+        else if (readString.startsWith("#2")) { // Checks readString if it starts with #2
+          feels = readString.substring(2); // Adds the readString less the first 2 characters to feels
+          Serial.println(feels); // Prints feels to serial
         }
         
-        else if (readString.startsWith("#3")) {
-          weather = readString.substring(2);
-          Serial.println(weather);
+        else if (readString.startsWith("#3")) { // Checks readString if it starts with #3
+          weather = readString.substring(2); // Adds the readString less the first 2 characters to weather
+          Serial.println(weather); // Prints weather to serial
         }
         
-        else if (readString.startsWith("#4")) {
-          lowest = readString.substring(2);
-          Serial.println(lowest);
+        else if (readString.startsWith("#4")) { // Checks readString if it starts with #4
+          lowest_temp = readString.substring(2); // Adds the readString less the first 2 characters to lowest_temp
+          Serial.println(lowest_temp); // Prints lowest_temp to serial
         }
         
-        else if (readString.startsWith("#5")) {
-          wind = readString.substring(2);
-          Serial.println(wind);
+        else if (readString.startsWith("#5")) { // Checks readString if it starts with #5
+          wind = readString.substring(2); // Adds the readString less the first 2 characters to wind
+          Serial.println(wind); // Prints wind to serial
         }
         
-        else if (readString.startsWith("#6")) {
-          lowest_time = readString.substring(2);
-          Serial.println(lowest_time);
+        else if (readString.startsWith("#6")) { // Checks readString if it starts with #6
+          lowest_time = readString.substring(2); // Adds the readString less the first 2 characters to lowest_time
+          Serial.println(lowest_time); // Prints lowest_time to serial
         }
         
-        else if (readString.startsWith("#7")) {
-          highest_temp = readString.substring(2);
-          Serial.println(highest_temp);
+        else if (readString.startsWith("#7")) { // Checks readString if it starts with #7
+          highest_temp = readString.substring(2); // Adds the readString less the first 2 characters to highest_temp
+          Serial.println(highest_temp); // Prints highest_temp to serial
         }
         
-        else if (readString.startsWith("#8")) {
-          highest_time = readString.substring(2);
-          Serial.println(highest_time);
+        else if (readString.startsWith("#8")) { // Checks readString if it starts with #8
+          highest_time = readString.substring(2); // Adds the readString less the first 2 characters to highest_time
+          Serial.println(highest_time); // Prints highest_time to serial
         }
         readString=""; //clears variable for new input
       }
     }
     
-    else if (c =='@') {
-      if (readString.length() >0) {
-        if (readString == "blue") {
-          setColor(0, 0, 255);  // blue
+    else if (c =='@') { // Check if c contains @
+      if (readString.length() >0) { // Checks that the length of readString is over 0
+        if (readString == "blue") { // Check if readString contains blue
+          setColor(0, 0, 255); // Sets the RGB colour to blue
         }
-        else if (readString == "green") {
-          setColor(0, 255, 0);  // green
+        else if (readString == "green") { // Check if readString contains green
+          setColor(0, 255, 0); // Sets the RGB colour to green
         }
-        else if (readString == "red") {
-          setColor(255, 0, 0);  // red
+        else if (readString == "red") { // Check if readString contains red
+          setColor(255, 0, 0);  // Sets the RGB colour to red
         }
-        else if (readString == "yellow") {
-          setColor(255, 255, 0);  // yellow
+        else if (readString == "yellow") { // Check if readString contains yellow
+          setColor(255, 255, 0); // Sets the RGB colour to yellow
         }
-        else if (readString == "purple") {
-          setColor(175, 0, 175);  // purple
+        else if (readString == "purple") { // Check if readString contains purple
+          setColor(175, 0, 175); // Sets the RGB colour to purple
         }
-        else if (readString == "aqua") {
-          setColor(0, 255, 255);  // aqua
-      
+        else if (readString == "aqua") { // Check if readString contains aqua
+          setColor(0, 255, 255); // Sets the RGB colour to aqua
+          
     }
     }
     readString=""; //clears variable for new input
@@ -164,36 +159,32 @@ void loop() {
     }
   }
   // -------------- Debound code start --------------
-  int reading = digitalRead(button);
-  buttonState1 = digitalRead(button);
+  int reading = digitalRead(button); // Load button stat to int reading
+  buttonState1 = digitalRead(button); // Load button stat into buttonState1
 
-  if (reading != lastButtonState) {
-    lastDebounceTime = millis();
+  if (reading != lastButtonState) { // Check if reading doesnt equal lastButtonState
+    lastDebounceTime = millis(); 
   } 
   
   if ((millis() - lastDebounceTime) > debounceDelay) {
 
     if (reading != buttonState) {
-      buttonState = reading;
+      buttonState = reading; // 
       
-        if (buttonState1 != lastButtonState1) {
+        if (buttonState1 != lastButtonState1) { // Check buttonState1 doesnt equal lastButtonState1
           
-          if (buttonState1 == LOW) {
-            buttonPushCounter1++;
+          if (buttonState1 == LOW) { // Check if buttonStata1 == LOW
+            buttonPushCounter1++; // Increment buttonPushCounter1
             
-            if (buttonPushCounter1 == 9) {
-              buttonPushCounter1 = 1;}
-              lcd.clear();
-            //Serial.println("LCD clear");
-            //Serial.println("on");
-            //Serial.print("number of button pushes:  ");
-            //Serial.println(buttonPushCounter1, DEC);
+            if (buttonPushCounter1 == 9) { // Check buttonPushCounter1 is == 9
+              buttonPushCounter1 = 1;} // If buttonPushCounter == 9 then set buttonPushCounter1 to 1
+              lcd.clear(); // Clear the lcd
           }
           else {
-            //Serial.println("off"); 
+            Serial.println("off"); 
           }
         }
-          lastButtonState1 = buttonState1;
+          lastButtonState1 = buttonState1; 
     }
   }
   lastButtonState = reading;
@@ -201,8 +192,8 @@ void loop() {
   
     if (buttonPushCounter1 == 1) {
       lcd.print("--Current temp--");
-      lcd.setCursor(0,1);
-      lcd.print(temp_c);
+      lcd.setCursor(0,1); // Set cursor to start of the second screen
+      lcd.print(temp_c); // Lcd print temp_c string
       //lcd.print(char(0));
       //lcd.write(165);
       lcd.print("C");
@@ -210,40 +201,40 @@ void loop() {
     
     if (buttonPushCounter1 == 2) {
       lcd.print("---Feels like---");
-      lcd.setCursor(0,1);
-      lcd.print(feels);
+      lcd.setCursor(0,1); // Set cursor to start of the second screen
+      lcd.print(feels); // Lcd print feels string
       //lcd.print(char(0));
       lcd.print("C");
     }
 
     if (buttonPushCounter1 == 3) {
       lcd.print("-----Weather----");
-      lcd.setCursor(0,1);
-      lcd.print(weather);
+      lcd.setCursor(0,1); // Set cursor to start of the second screen
+      lcd.print(weather); // Lcd print weather String
     }
 
     if (buttonPushCounter1 == 4) {
       lcd.print("---Wind speed---");
-      lcd.setCursor(0,1);
-      lcd.print(wind);
+      lcd.setCursor(0,1); // Set cursor to start of the second screen
+      lcd.print(wind); // Lcd print wind string
     }
 
     if (buttonPushCounter1 == 5) {
       lcd.print("--Lowest temp---");
-      lcd.setCursor(0,1);
-      lcd.print(lowest_time);
+      lcd.setCursor(0,1); // Set cursor to start of the second screen
+      lcd.print(lowest_time); // Lcd print lowest_time string
       lcd.print(" ");
-      lcd.print(lowest);
+      lcd.print(lowest_temp); // Lcd print lowest_temp string
       //lcd.print(char(0));
       lcd.print("C");
     }
     
     if (buttonPushCounter1 == 6) {
       lcd.print("--Highest temp--");
-      lcd.setCursor(0,1);
-      lcd.print(highest_time);
+      lcd.setCursor(0,1); // Set cursor to start of the second screen
+      lcd.print(highest_time); // Lcd print highest_time string
       lcd.print(" ");
-      lcd.print(highest_temp);
+      lcd.print(highest_temp); // Lcd print highest_temp string
       //lcd.print(char(0));
       lcd.print("C");
     }
@@ -251,13 +242,12 @@ void loop() {
 
   if (buttonPushCounter1 == 7) {
           
-    char buffer_minutes[3];
-      char buffer_seconds[3];
-      //lcd.clear();
-      lcd.setCursor(0,0);
-      readTime();
+    char buffer_minutes[3]; // Creates a char buffer of 3 characters for minutes
+      char buffer_seconds[3]; // Creates a char buffer of 3 characters for seconds
+      lcd.setCursor(0,0); // Set lcd cursor to start of first line
+      readTime(); // Runs readTime to get RTC data
       lcd.print(days[weekday-1]);
-      lcd.setCursor(0,1);
+      lcd.setCursor(0,1); // Set lcd cursor to start of second line
       lcd.print(monthday);
       lcd.print("/");
       lcd.print(month);
@@ -266,10 +256,10 @@ void loop() {
       lcd.print(" ");
       lcd.print(hour);
       lcd.print(":");
-      sprintf(buffer_minutes, "%02d", minute);
+      sprintf(buffer_minutes, "%02d", minute); // Floats the minutes to only 2 decimal points stores them on buffer
       lcd.print(buffer_minutes);
       lcd.print(":");
-      sprintf(buffer_seconds, "%02d", second);
+      sprintf(buffer_seconds, "%02d", second); // Floats the seconds to only 2 decimal points stores them on buffer
       lcd.print(buffer_seconds);
   }
   
@@ -278,17 +268,17 @@ void loop() {
       if (millis() >= waitUntilcycle1) {
       lcd.clear();
       lcd.print("--Current temp--");
-      lcd.setCursor(0,1);
-      lcd.print(temp_c);
+      lcd.setCursor(0,1); // Ser cursor to start of second line
+      lcd.print(temp_c); // Lcd print temp_c string
       //lcd.print(char(0));
       lcd.print("C");
-      waitUntilcycle1 += 35000;  
+      waitUntilcycle1 += 35000; // Addes 35000 to waitUntilcycle1
       }
     if (millis() >= waitUntilcycle2) {
       lcd.clear();
-      lcd.print("---Feels like---");
-      lcd.setCursor(0,1);
-      lcd.print(feels);
+      lcd.print("---Feels like---"); // Lcd print feels string
+      lcd.setCursor(0,1); // Ser cursor to start of second line
+      lcd.print(feels); // Lcd print feels String
       //lcd.print(char(0));
       lcd.print("C");
       waitUntilcycle2 += 35000;
@@ -296,24 +286,24 @@ void loop() {
     if (millis() >= waitUntilcycle3) {
       lcd.clear();
       lcd.print("-----Weather----");
-      lcd.setCursor(0,1);
-      lcd.print(weather);
+      lcd.setCursor(0,1); // Ser cursor to start of second line
+      lcd.print(weather); // Lcd print weather String
       waitUntilcycle3 += 35000;
      }
     if (millis() >= waitUntilcycle4) {
       lcd.clear();
       lcd.print("---Wind speed---");
-      lcd.setCursor(0,1);
-      lcd.print(wind);
+      lcd.setCursor(0,1); // Ser cursor to start of second line
+      lcd.print(wind); // Lcd print wind string
       waitUntilcycle4 += 35000;
     }
     if (millis() >= waitUntilcycle5) {
       lcd.clear();
       lcd.print("--Lowest temp---");
-      lcd.setCursor(0,1);
-      lcd.print(lowest_time);
+      lcd.setCursor(0,1); // Ser cursor to start of second line
+      lcd.print(lowest_time); // Lcd print lowest_time string
       lcd.print(" ");
-      lcd.print(lowest);
+      lcd.print(lowest_temp); // Lcd print lowest_temp string
       //lcd.print(char(0));
       lcd.print("C");
       waitUntilcycle5 += 35000;
@@ -321,22 +311,22 @@ void loop() {
     if (millis() >= waitUntilcycle6) {
       lcd.clear();
       lcd.print("--Highest temp--");
-      lcd.setCursor(0,1);
-      lcd.print(highest_time);
+      lcd.setCursor(0,1); // Ser cursor to start of second line
+      lcd.print(highest_time); // Lcd print highest_time string
       lcd.print(" ");
-      lcd.print(highest_temp);
+      lcd.print(highest_temp); // Lcd print highest_temp string
       //lcd.print(char(0));
       lcd.print("C");
       waitUntilcycle6 += 35000;
     }
     if (millis() >= waitUntilcycle7) {
-      char buffer_minutes[3];
-      char buffer_seconds[3];
+      char buffer_minutes[3]; // Creates a char buffer of 3 characters for minutes
+      char buffer_seconds[3]; // Creates a char buffer of 3 characters for seconds
       lcd.clear();
-      lcd.setCursor(0,0);
-      readTime();
+      lcd.setCursor(0,0); // Set lcd cursor to start of first line
+      readTime(); // Runs readTime to get RTC data
       lcd.print(days[weekday-1]);
-      lcd.setCursor(0,1);
+      lcd.setCursor(0,1); // Set lcd cursor to start of second line
       lcd.print(monthday);
       lcd.print("/");
       lcd.print(month);
@@ -345,13 +335,14 @@ void loop() {
       lcd.print(" ");
       lcd.print(hour);
       lcd.print(":");
-      sprintf(buffer_minutes, "%02d", minute);
+      sprintf(buffer_minutes, "%02d", minute); // Floats the minutes to only 2 decimal points stores them on buffer
       lcd.print(buffer_minutes);
       waitUntilcycle7 += 35000;
   }
   }
 }
 
+// Set RGB colours
 void setColor(int red, int green, int blue)
 {
   #ifdef COMMON_ANODE
@@ -364,31 +355,7 @@ void setColor(int red, int green, int blue)
   analogWrite(bluePin, blue);  
 }
 
-/*void printTime() {
-  char buffer[3];
-  const char* AMPM = 0;
-  readTime();
-  Serial.print(days[weekday-1]);
-  Serial.print(" ");
-  Serial.print(months[month-1]);
-  Serial.print(" ");
-  Serial.print(monthday);
-  Serial.print(", 20");
-  Serial.print(year);
-  Serial.print(" ");
-  if (hour > 12) {
-    hour -= 12;
-    AMPM = " PM";
-  }
-  else AMPM = " AM";
-  Serial.print(hour);
-  Serial.print(":");
-  sprintf(buffer, "%02d", minute);
-  Serial.print(buffer);
-  Serial.println(AMPM);
-}*/
-
-
+// RTC data
 void readTime() {
   Wire.beginTransmission(DS1307);
   Wire.write(byte(0));
